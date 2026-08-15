9819b0a8c71e21c765807cd25800d80d687efa57
leusiojosefilimone <leusiojosefilimone@gmail.com>
2026-08-15 16:15:57 +0200

    ajustes para deploy

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -10,7 +10,7 @@ import axios from "axios";
 import Place from "./components/pages/Place";
 
 
-axios.defaults.baseURL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "http://localhost:3000/api";
+axios.defaults.baseURL = import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://hashbnb-24q5.onrender.com/";
 axios.defaults.withCredentials = true;
 console.log(import.meta.env);
 
